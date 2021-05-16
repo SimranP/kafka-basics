989f23ddb93d7511a3db45c416ef865720bfe5ce
Simran <simranpalmeerut@gmail.com>
2021-05-16 19:47:48 +0530

    Produce multiple messages and log meta

--- src/main/java/com/github/simranp/demo/ProducerDemo.java
+++ src/main/java/com/github/simranp/demo/ProducerDemo.java
@@ -1,9 +1,9 @@
 package com.github.simranp.demo;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
@@ -11,6 +11,8 @@ import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 public class ProducerDemo {
   public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
     //create properties
     Properties properties = new Properties();
     String bootstrapServers = "127.0.0.1:9092";
@@ -20,14 +22,24 @@ public class ProducerDemo {
 
     //create kafka producer
     KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+    for (int i = 0; i < 12; i++) {
+      //create producer record
 
-    //create producer record
-    ProducerRecord<String, String> message = new ProducerRecord<>("hello_world", "Hi, I am Java Producer");
+      ProducerRecord<String, String> message = new ProducerRecord<>("hello_world", "Hi, I am Java Producer");
 
-    //send message
-    kafkaProducer.send(message);
+      //send message
+      kafkaProducer.send(message, (metadata, exception) -> {
+        if (exception == null) {
+          logger.info("Successfully produced message on --->" + metadata.toString());
+        } else {
+          logger.error("Error received", exception);
+        }
+      });
+    }
 
     //flush message
+    kafkaProducer.flush();
+    //close producer
     kafkaProducer.close();
   }
 }
