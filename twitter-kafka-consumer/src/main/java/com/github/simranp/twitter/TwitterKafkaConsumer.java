a8e3b81bec6ed5157e4f6b4faf621f48654d3616
Simran <simranpalmeerut@gmail.com>
2021-05-20 10:16:37 +0530

    Bulk insert 100 records

--- twitter-kafka-consumer/src/main/java/com/github/simranp/twitter/TwitterKafkaConsumer.java
+++ twitter-kafka-consumer/src/main/java/com/github/simranp/twitter/TwitterKafkaConsumer.java
@@ -6,9 +6,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
@@ -20,6 +20,7 @@ import java.util.Properties;
 
 import static com.github.simranp.twitter.ElasticSearchClient.createClient;
 import static java.util.Set.of;
+import static org.elasticsearch.client.RequestOptions.DEFAULT;
 
 public class TwitterKafkaConsumer {
   public static final String TOPIC_NAME = "twitter_tweets";
@@ -37,7 +38,7 @@ public class TwitterKafkaConsumer {
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
     properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
 
     // create consumer
     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
@@ -48,34 +49,42 @@ public class TwitterKafkaConsumer {
   private void run() throws IOException {
     RestHighLevelClient client = createClient();
     KafkaConsumer<String, String> consumer = createConsumer();
-    IndexRequest indexRequest = new IndexRequest("twitter");
+
 
     while (true) {
       ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-      logger.info("Received "+records.count()+ " records");
+      BulkRequest bulkRequest = new BulkRequest();
+      IndexRequest indexRequest = new IndexRequest("twitter");
+
+      int recordsCount = records.count();
+      logger.info("Received " + recordsCount + " records");
+
       for (ConsumerRecord<String, String> record : records) {
-        indexRequest
-          .source(record.value(), XContentType.JSON)
-          .id(extractIdFromTweet(record.value()));
-        IndexResponse index = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = index.getId();
-        logger.info(id);
+        try {
+          indexRequest
+            .source(record.value(), XContentType.JSON)
+            .id(extractIdFromTweet(record.value()));
+          bulkRequest.add(indexRequest);
+        } catch (NullPointerException e) {
+          logger.warn("Received bad data " + record.value());
+        }
       }
-      logger.info("Committing offsets...");
-      consumer.commitSync();
-      logger.info("Offsets have been committed");
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+
+      if (recordsCount > 0) {
+        BulkResponse bulkItemResponses = client.bulk(bulkRequest, DEFAULT);
+        logger.info("Bulk Response: " + bulkItemResponses.status().toString() + " with " + bulkItemResponses.getItems().length + " records");
+        logger.info("Committing offsets...");
+        consumer.commitSync();
+        logger.info("Offsets have been committed");
       }
     }
 
 //    client.close();
   }
 
+  JsonParser jsonParser = new JsonParser();
+
   private String extractIdFromTweet(String record) {
-    JsonParser jsonParser = new JsonParser();
     return jsonParser.parse(record)
       .getAsJsonObject()
       .get("id_str")
